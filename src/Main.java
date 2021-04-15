d1415ee5214e855270156f881d3bc781936aa28d
Bingoliath <uldis951@gmail.com>
2021-04-15 16:54:57 +0300

    Secondary branch set-up

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // TODO: Jauzraksta kods
+        // Otrs zars, nav galvenais :)
     }
 }
