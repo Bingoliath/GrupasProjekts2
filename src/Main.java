8384092462a677b3a831f429e4e8598d81a46479
okristapans <oskars.krsitapans@gmail.com>
2021-04-15 16:33:30 +0300

    tests

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("maneemejeff");
-        System.out.println("branch?");
-        //yaaaaaaaaas
-        //blablabla
+        System.out.println("Sveik pasaul!");
     }
 }
